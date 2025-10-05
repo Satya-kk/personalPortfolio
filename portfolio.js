a10039bde2a756d6191a8f8dff53e85a1954b85b
Satyam Kadam <satyam.k.kadam@gmail.com>
2025-10-05 11:29:14 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+//web developer name animation
+let typed = new Typed(".txt",{
+    strings: ["Frontend Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
